03a58c0608f9b71660e39906a9884ffc09c643a3
Raymond Lopez <raymondlopez13@yahoo.com>
2021-06-14 16:26:44 -0500

    added max width to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
             <img src={headshot} alt="headshot" />
             <div className="about-text">
                 <p className='aboutP'>
-                    I am a 22 year old full stack developer with a certificate from The University of Texas.
+                Hello I’m Raymond, a full stack web developer with a certificate in Full Stack Web Development from the University of Texas at Austin. I love to build beautiful, functional websites using MERN, SQL, HTML, and CSS. I have great time management skills and thrive in high stress situations. I can excel in a leadership role, or as a teammate because of my ability to communicate the team goals and best ways to achieve them. I would love to work for a company that is looking to help people build their dream website for their business, portfolio, or anything else that they need!
                 </p>
             </div>
         </section>
