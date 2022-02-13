48b06209778ede1ee5c46e9b92047546ffc8dcd7
Raymond Lopez <raymondlopez13@yahoo.com>
2022-02-13 16:10:11 -0600

    Started adding nav modal

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,25 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 function Nav() {
     const location = useLocation();
+    const [ navOpen, setNavOpen ] = useState(false);
     useEffect(() => {
         console.log(location.pathname);
     }, [location])
-    const menu = () => {
-        const links = document.getElementById('nav-link-container');
-        links.setAttribute('style', 'display: flex !important');
-    }
-    const close = () => {
-        const links = document.getElementById('nav-link-container');
-        if(window.screen.width < 901) links.setAttribute('style', 'display: none !important');
+    const click = () => {
+        if(navOpen) {
+            setNavOpen(false);
+        } else {
+            setNavOpen(true);
+        }
     }
     return (
         <>
             {location.pathname === '/' ? null : (
-                <nav>
-                    Tis going to be the navbar
-                </nav>
+                <>
+                {navOpen ?
+                    <div className='nav'>
+                        <button className='close-menu-btn' onClick={click}>
+                            <AiOutlineClose/>
+                        </button>
+                        <div className='link-container'>
+                            These are some links
+                        </div>
+                    </div>
+                    :
+                    <button className='menu-btn' onClick={click}>
+                        <AiOutlineMenu/>
+                    </button>
+                }
+                </>  
             )}
         </>
     )
